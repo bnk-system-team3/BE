32f4ef009e23d1a3a55a1c3e7c2ca8f18dac4ee6
jeongwooshin <jeongwooshin.0414@gmail.com>
2023-12-19 09:50:03 +0900

    <Update> Dummy Data Query

--- houssg/target/classes/LanguageCategoryDummy.sql
+++ houssg/target/classes/LanguageCategoryDummy.sql
@@ -1,6 +1,6 @@
-INSERT INTO languagecategory
-VALUES (1, "C"), (2, "C++"), (3, "C#"), (4, "Java"), (5, "JavaScript"), (6, "Python"), (7, "Spring"), 
-(8, "MySQL"), (9, "MSSQL"), (10, "Next.js"), (11, "React"), (12, "TypeScript"), (13, "Vue"), (14, "Node.js"),
-(15, "Nest.js"), (16, "Express"), (17, "Go"), (18, "Django"), (19, "Swift"), (20, "Kotlin"), (21, "MongoDB"),
-(22, "PHP"), (23, "GraphQL"), (24, "FireBase"), (25, "ReactNative"), (26, "Unity"), (27, "Flutter"), (28, "AWS"),
-(29, "Kubernetes"), (30, "Docker"), (31, "Git"), (32, "Figma"), (33, "Zeplin");
+INSERT INTO languagecategory (languageName)
+VALUES ("C"), ("C++"), ("C#"), ("Java"), ("JavaScript"), ("Python"), ("Spring"), 
+("MySQL"), ("MSSQL"), ("Next.js"), ("React"), ("TypeScript"), ("Vue"), ("Node.js"),
+("Nest.js"), ("Express"), ("Go"), ("Django"), ("Swift"), ("Kotlin"), ("MongoDB"),
+("PHP"), ("GraphQL"), ("FireBase"), ("ReactNative"), ("Unity"), ("Flutter"), ("AWS"),
+("Kubernetes"), ("Docker"), ("Git"), ("Figma"), ("Zeplin");
--- houssg/target/classes/PositionCategoryDummy.sql
+++ houssg/target/classes/PositionCategoryDummy.sql
@@ -1,3 +1,3 @@
-INSERT INTO positioncategory
-VALUES (1, "프론트엔드"), (2, "백엔드"), (3, "디자이너"), (4, "IOS"), (5, "안드로이드"),
-(6, "데브옵스"), (7, "PM"), (8, "기획자");
+INSERT INTO positioncategory (positionName)
+VALUES ("프론트엔드"), ("백엔드"), ("디자이너"), ("IOS"),
+("안드로이드"),("데브옵스"), ("PM"), ("기획자");
