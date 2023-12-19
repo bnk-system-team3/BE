8f9d68115731fd73d58e71844a7461a1abafba2d
0-ssg <dudtjr001@gmail.com>
2023-12-19 09:56:11 +0900

    sql upload

---
+++ houssg/src/main/resources/LanguageCategoryDummy.sql
@@ -0,0 +1,6 @@
+INSERT INTO languagecategory (languageName)
+VALUES ("C"), ("C++"), ("C#"), ("Java"), ("JavaScript"), ("Python"), ("Spring"), 
+("MySQL"), ("MSSQL"), ("Next.js"), ("React"), ("TypeScript"), ("Vue"), ("Node.js"),
+("Nest.js"), ("Express"), ("Go"), ("Django"), ("Swift"), ("Kotlin"), ("MongoDB"),
+("PHP"), ("GraphQL"), ("FireBase"), ("ReactNative"), ("Unity"), ("Flutter"), ("AWS"),
+("Kubernetes"), ("Docker"), ("Git"), ("Figma"), ("Zeplin");
---
+++ houssg/src/main/resources/PositionCategoryDummy.sql
@@ -0,0 +1,3 @@
+INSERT INTO positioncategory (positionName)
+VALUES ("프론트엔드"), ("백엔드"), ("디자이너"), ("IOS"),
+("안드로이드"),("데브옵스"), ("PM"), ("기획자");
